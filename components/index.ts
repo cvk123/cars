11df8615e3999fa6f1627505f66b1bf37358b2c2
cvk123 <pavelvintr11@seznam.cz>
2023-06-16 14:42:09 +0200

    first commit

---
+++ components/index.ts
@@ -0,0 +1,25 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton"
+import Navbar  from "./Navbar";
+import Footer from "./Footer";
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CardDetails from "./CardDetails";
+import ShowMore from "./ShowMore";
+
+export {
+ Hero,
+ CustomButton,
+ Navbar,
+ Footer,
+ SearchBar,
+ CustomFilter,
+ SearchManufacturer,
+ CarCard,
+ CardDetails,
+ ShowMore
+}
+
+
---
+++ constants/index.ts
@@ -0,0 +1,102 @@
+export const manufacturers = [
+    "Acura",
+    "Alfa Romeo",
+    "Aston Martin",
+    "Audi",
+    "Bentley",
+    "BMW",
+    "Buick",
+    "Cadillac",
+    "Chevrolet",
+    "Chrysler",
+    "Citroen",
+    "Dodge",
+    "Ferrari",
+    "Fiat",
+    "Ford",
+    "GMC",
+    "Honda",
+    "Hyundai",
+    "Infiniti",
+    "Jaguar",
+    "Jeep",
+    "Kia",
+    "Lamborghini",
+    "Land Rover",
+    "Lexus",
+    "Lincoln",
+    "Maserati",
+    "Mazda",
+    "McLaren",
+    "Mercedes-Benz",
+    "MINI",
+    "Mitsubishi",
+    "Nissan",
+    "Porsche",
+    "Ram",
+    "Rolls-Royce",
+    "Subaru",
+    "Tesla",
+    "Toyota",
+    "Volkswagen",
+    "Volvo",
+  ];
+  
+  export const yearsOfProduction = [
+    { title: "Year", value: "" },
+    { title: "2014", value: "2014" },
+    { title: "2015", value: "2015" },
+    { title: "2016", value: "2016" },
+    { title: "2017", value: "2017" },
+    { title: "2018", value: "2018" },
+    { title: "2019", value: "2019" },
+    { title: "2020", value: "2020" },
+    { title: "2021", value: "2021" },
+    { title: "2022", value: "2022" },
+    { title: "2023", value: "2023" },
+  ];
+  
+  export const fuels = [
+    {
+      title: "Fuel",
+      value: "",
+    },
+    {
+      title: "Gas",
+      value: "Gas",
+    },
+    {
+      title: "Electricity",
+      value: "Electricity",
+    },
+  ];
+  
+  export const footerLinks = [
+    {
+      title: "About",
+      links: [
+        { title: "How it works", url: "https://www.youtube.com/watch?v=sbpnF8Kczvc" },
+        { title: "Featured", url: "/" },
+        { title: "Partnership", url: "/" },
+        { title: "Bussiness Relation", url: "/" },
+      ],
+    },
+    {
+      title: "Company",
+      links: [
+        { title: "Events", url: "/" },
+        { title: "Blog", url: "https://remarkable-khapse-e52934.netlify.app/" },
+        { title: "Podcast", url: "/" },
+        { title: "Invite a friend", url: "/" },
+      ],
+    },
+    {
+      title: "Socials",
+      links: [
+        { title: "Discord", url: "https://discordapp.com/users/514137529801244672" },
+        { title: "Instagram", url: "https://www.instagram.com/pavel.vintr/" },
+        { title: "Twitter", url: "/" },
+        { title: "Facebook", url: "https://www.facebook.com/pavel.vintr.39/" },
+      ],
+    },
+  ];
